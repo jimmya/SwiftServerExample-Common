b9e40530043ba77f534057051fd979d5af6087f0
Jimmy Arts <arts.jimmy@gmail.com>
2016-11-15 09:08:20 +0100

    Added SwiftyJSON dependency and first Model

--- Package.swift
+++ Package.swift
@@ -4,5 +4,8 @@ let package = Package(
     name: "SwiftServerExample-Common",
     targets : [
         Target(name: "SwiftExampleCommon")
+    ],
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 15)
     ]
 )
---
+++ Sources/SwiftExampleCommon/User.swift
@@ -0,0 +1,44 @@
+import Foundation
+import SwiftyJSON
+
+public final class User: NSObject, NSCoding {
+    
+    var name: String?
+    var email: String?
+    var password: String?
+    var id: String?
+    
+    override init() {
+        self.name = nil
+        self.email = nil
+        self.password = nil
+        self.id = nil
+    }
+    
+    public init?(jsonData: JSON) {
+        guard let name = jsonData["name"].string,
+            let email = jsonData["email"].string,
+            let id = jsonData["id"].string else {
+            return nil
+        }
+        self.name = name
+        self.email = email
+        self.id = id
+        self.password = jsonData["password"].string
+    }
+    
+    public required convenience init(coder decoder: NSCoder) {
+        self.init()
+        name = decoder.decodeObject(forKey: "name") as? String
+        email = decoder.decodeObject(forKey: "email") as? String
+        password = decoder.decodeObject(forKey: "password") as? String
+        id = decoder.decodeObject(forKey: "id") as? String
+    }
+    
+    public func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(email, forKey: "email")
+        aCoder.encode(password, forKey: "password")
+        aCoder.encode(id, forKey: "id")
+    }
+}
